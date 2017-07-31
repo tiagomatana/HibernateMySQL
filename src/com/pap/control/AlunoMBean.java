39d5d9708d7e14b8ab40de3ae31d3d6222708f77
Tiago Matana <tiago.matana@gmail.com>
2017-07-31 12:39:05 -0300

    Testado no UBUNTU (funcionando)

---
+++ src/com/pap/control/AlunoMBean.java
@@ -0,0 +1,70 @@
+package com.pap.control;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import com.pap.dao.AlunoDao;
+import com.pap.entity.Aluno;
+
+@ManagedBean(name = "alunoMBean")
+@SessionScoped
+
+
+public class AlunoMBean implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    private Aluno aluno;
+    private AlunoDao ad;
+    private List<Aluno> AlunoList;
+    
+    @PostConstruct
+    public void init(){
+        this.aluno = new Aluno();
+        this.ad = new AlunoDao();
+        this.AlunoList = ad.list();
+    }
+    public String newAluno(){
+        return "manageAluno";
+    }
+    
+    public String delete(Aluno aluno){
+        ad.delete(aluno);
+        return "aluno";
+    }
+    public String edit(Aluno aluno){
+        this.aluno = aluno;
+        return "manageAluno";
+    }
+    
+    public String save() {
+        ad.saveOrUpdate(aluno);
+        this.aluno = new Aluno();
+        return "aluno";
+    }
+    
+    //Geters and seters
+     
+    public Aluno getAluno() {
+        return aluno;
+    }
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+    public AlunoDao getAd() {
+        return ad;
+    }
+    public void setAd(AlunoDao ad) {
+        this.ad = ad;
+    }
+    public List<Aluno> getAlunoList() {
+        this.AlunoList = ad.list();
+        return AlunoList;
+    }
+    public void setAlunoList(List<Aluno> alunoList) {
+        AlunoList = alunoList;
+    }
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+}
